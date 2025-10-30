f29f846af47c17a7509039d03629405b8ae1f2ec
Jun <imlijunda@gmail.com>
2025-10-30 15:03:58 +1030

    FiLM block

--- block/__init__.py
+++ block/__init__.py
@@ -3,11 +3,13 @@ from .trend_loss import MultiScaleTrendLoss, MultiScaleTrendDirectionLoss
 from .temporal_mix import TemporalMixBlock, TemporalMixStack
 from .feature_mix import FeatureMixBlock
 from .forecast import AutoregressiveHead, DirectForecastHead
+from .film import FiLMBlock
 
 __all__ = ["TransBlock",
            "TransStack",
            "MultiScaleTrendLoss",
            "MultiScaleTrendDirectionLoss",
+           "FiLMBlock",
            "FeatureMixBlock",
            "TemporalMixBlock",
            "TemporalMixStack",
---
+++ block/film.py
@@ -0,0 +1,33 @@
+from typing import Tuple
+import torch
+from torch import nn
+
+from layer import Linear
+
+
+class FiLMBlock(nn.Module):
+    def __init__(self,
+                 film_dim: int,
+                 film_hidden_size: int,
+                 model_hidden_size: int,
+                 film_dropout: float = 0.0):
+        super().__init__()
+
+        self.film_mlp = nn.Sequential(
+            Linear(in_features=film_dim,
+                   out_features=film_hidden_size,
+                   bias=True),
+            nn.GELU(),
+            nn.Dropout(film_dropout),  # only dropout if too dense
+            Linear(in_features=film_hidden_size,
+                   out_features=model_hidden_size * 2,
+                   bias=False)
+        )
+
+    def forward(self, film_cond: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+        """
+        [B, film_dim] -> [B, model_hidden_size] x2 (gamma, beta)
+        """
+        params = self.film_mlp(film_cond)
+        gamma, beta = params.chunk(2, dim=-1)
+        return gamma, beta
